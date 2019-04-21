8f13ee0e96e9970f7c77dc2123f52f1e95a3ea16
Mohammad Butt <mbutt@student.42.us.org>
2019-04-20 22:58:08 -0700

    updating files

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,13 @@
 /*   By: mbutt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/17 12:17:07 by mbutt             #+#    #+#             */
-/*   Updated: 2019/04/20 21:04:08 by mbutt            ###   ########.fr       */
+/*   Updated: 2019/04/20 22:57:45 by mbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int		get_next_line(const int fd, char **one_line)
+int		get_next_line(const int fd, char **line)
 {
 	int			len;
 	char		placeholder[BUFF_SIZE + 1];
@@ -44,7 +44,7 @@ int		get_next_line(const int fd, char **one_line)
 		return (0);
 	while (s[fd][len] != '\0' && s[fd][len] != '\n')
 		len++;
-	*one_line = ft_strsub(s[fd], 0, len);
+	*line = ft_strsub(s[fd], 0, len);
 	temp = ft_strdup(&*(s[fd] + len + 1));
 	ft_memdel((void **)&s[fd]);
 	s[fd] = ft_strdup(temp);
@@ -52,19 +52,19 @@ int		get_next_line(const int fd, char **one_line)
 	return (1);
 }
 /*
-**int main (void)
-**{
-**	int fd;
-**	int ret;
-**	char *one_line;
-**	ret = 0;
-**	fd = 0;
-**	fd = open("test7_3.txt", O_RDONLY);
-**	while ((ret = get_next_line(fd, &one_line) > 0))
-**	{
-**		printf("fd should be 1:|%d|\n", ret);
-**		printf("|%s|\n\n", one_line);
-**		free(one_line);
-**	}
-**}
+int main (void)
+{
+	int fd;
+	int ret;
+	char *one_line;
+	ret = 0;
+	fd = 0;
+	fd = open("test7_3.txt", O_RDONLY);
+	while ((ret = get_next_line(fd, &one_line) > 0))
+	{
+		printf("fd should be 1:|%d|\n", ret);
+		printf("|%s|\n\n", one_line);
+		free(one_line);
+	}
+}
 */
